10dbbcc76e25bc7d3b93d96568fd2ffe1060642b
Diana Yakusheva <diana.yakushevaa@gmail.com>
2022-08-27 21:27:27 +0300

    Add header

---
+++ src/Header.js
@@ -0,0 +1,8 @@
+export default function Header(){
+    return (
+        <div className="header">
+            <h1 className="header--title">Greeting Card Generator</h1>
+            <h2 className="header--quote">make your personal congratulations</h2>
+        </div>
+    )
+}
